9c0f5b6e63ee72d3807db4630f1713dd57ca8a20
Victor Wolmeister <wolmeistervictor@gmail.com>
2021-10-14 00:12:48 -0300

    Implement initial soap method

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,41 @@
 import express from 'express';
+import { listen as soapListen, IServices } from 'soap';
+import { v4 as uuid } from 'uuid';
+import { readFileSync } from 'fs';
 
 const app = express();
 
+type AddEventRequest = {
+  name: string;
+  location: string;
+};
+
+type AddEventResponse = {
+  id: string;
+  name: string;
+  location: string;
+};
+
+const soapServices: IServices = {
+  EventService: {
+    EventPort: {
+      addEvent(args: AddEventRequest): AddEventResponse {
+        console.log('Add event', args);
+        return {
+          id: uuid(),
+          ...args,
+        };
+      },
+    },
+  },
+};
+
+const xml = readFileSync(
+  'C:\\Users\\Devel\\Desktop\\Projects\\School\\architecture\\task4-ts\\src\\services.wsdl'
+).toString('utf-8');
+
 app.listen(3000, () => {
-  console.log('App running at http://localhost:3000');
+  soapListen(app, '/wsdl', soapServices, xml, () => {
+    console.log('App running at http://localhost:3000');
+  });
 });
